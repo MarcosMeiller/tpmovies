9b820a7acbc979468239e48855ce5295442db1ec
MarcosMeiller <mar.quiton@hotmail.com>
2020-09-29 20:16:48 -0300

    0.2

--- Models/cinema.php
+++ Models/cinema.php
@@ -4,21 +4,26 @@ namespace models;
 
 class cinema{
 
-    $capacity;
-    $name;
-    $location;
+    private $capacity;
+    private $name;
+    private $location;
+    private $room;
 
     public function __construct( $capacity,$name,$location){
+        $room = array();
         $this->capacity = $capacity;
         $this->name = $name;
         $this->location = $location;
     }
+    
     public function getLocation(){
         return $this->location;
     }
+
     public function getName(){
         return $this->name;
     }
+    
     public function getCapacity(){
         return $this->capacity;
     }
@@ -26,9 +31,11 @@ class cinema{
     public function setLocation($location){
         $this->location = $location;
     }
+    
     public function setName($name){
         $this->name = $name;
     }
+    
     public function setCapacity($capacity){
         $this->capacity = $capacity;
     }
